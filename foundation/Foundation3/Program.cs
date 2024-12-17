b285e35a426ca7e656ab823ba4b93577c9318054
dorestan268 <dorestan268@gmail.com>
2024-12-17 16:30:37 -0500

    Foundation03

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+       
+        Console.WriteLine("-----Fitness Activity Report-----");
+        Console.WriteLine();
+        
+
+        var activities = new List<Activity>
+        {
+            new RunningActivity(new DateTime(2024, 12, 1), 45, 5.0),
+            new CyclingActivity(new DateTime(2024, 12, 5), 35, 12.0), 
+            new SwimmingActivity(new DateTime(2024, 12, 10), 25, 40)  
+        };
+
+    
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+            Console.WriteLine();
+        }
     }
 }
